a801c2efa5d6d47a82cb1850cb91bb152ba6ec24
Rifat Biswas <rifatbiswas2@gmail.com>
2023-06-21 08:08:09 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const darkModeToggle = document.getElementById("darkModeToggle");
+    const body = document.body;
+  
+    darkModeToggle.addEventListener("change", function () {
+      body.classList.toggle("dark-mode", darkModeToggle.checked);
+    });
+  });
+  
